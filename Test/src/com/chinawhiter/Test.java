67c905a9fa131f24578d46bdad4c7d1eb21166f0
chenliang <chinawhiter@163.com>
2017-04-09 23:21:04 +0800

    wu

--- Test/src/com/chinawhiter/Test.java
+++ Test/src/com/chinawhiter/Test.java
@@ -4,9 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(123);
-		System.out.println(789);
-		System.out.println(456);
 	}
 
 }
